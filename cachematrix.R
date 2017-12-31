ef45a10240f88f0da0292a29b88854758cc7bb2c
Sushant Jaiswal <sushantjaiswal@gmail.com>
2017-12-31 12:00:01 +0530

    committing cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+# function makeCacheMatrix creates and returns a list of functions used by
+# another function cacheSolve to get or set the inverted matrix in cache
+# set() - set the value of the matrix
+# get() the value of the matrix
+# setInverse() - set the value of inverse of the matrix
+# getInverse() - get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # initialize to NULL
+    inv <- NULL
+    
+    # create the matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    # get the value of matrix
+    get <- function() x
+    
+    # invert the matrix and store in cache
+    setInverse <- function(inverse) inv <<- inverse
+    
+    # get the inverted matrix from cache
+    getInverse <- function() inv
+    
+    # return the list of functions
+    list(set=set, get=get, setinverse=setInverse, getinverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# function cacheSolve calcluates the inverse of the matrix 
+# which is created in function makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # get the inverse of the matrix stored in cache
+    inv <- x$getinverse()
+    
+    # if inverse of matrix is already calculated,
+    # then return it from cache
+    if(!is.null(inv)) {
+        message("Getting data from cache!")
+        return(inv)
+    }
+    
+    # otherwise, create the inverted matrix
+    data <- x$get()
+    inv <- solve(data)
+    
+    # set inverted matrix in cache
+    x$setinverse(inv)
+    
+    # show matrix
+    message("Computing inverted matrix and getting data!")
+    inv
 }
+
+# test run:
+# > x = rbind(c(1, 2), c(3, 4))
+# > m = makeCacheMatrix(x)
+# > m$get()
+# [,1] [,2]
+# [1,]    1    2
+# [2,]    3    4
+# > cacheSolve(m)
+# Computing inverted matrix and getting data!
+#     [,1] [,2]
+# [1,] -2.0  1.0
+# [2,]  1.5 -0.5
+# > cacheSolve(m)
+# Getting data from cache!
+#     [,1] [,2]
+# [1,] -2.0  1.0
+# [2,]  1.5 -0.5
